6bfc2e2133e129b9a004dccc978b77cc3ad544cc
Bounty.jpeg <jacobmayday@gmail.com>
2020-05-12 10:54:20 -0600

    Refactored splunk.js to use ES6 features, as well as fixed slow query

--- scripts/splunk.js
+++ scripts/splunk.js
@@ -1,50 +1,50 @@
-var locationInfo = document.location.pathname;
-var shardID;
-var globRootID;
-var masquerading;
-var currentUser;
-var garbageRemoval = ' AND action!=ping AND controller!=page_views AND action!=unread_count';
+const locationInfo = document.location.pathname;
+let shardID;
+let globRootID;
+let masquerading;
+let currentUser;
+const garbageRemoval = 'AND action!=ping AND controller!=page_views AND action!=unread_count';
 
-getPageInfo('https://' + document.domain);
+getPageInfo(`https://${document.domain}`);
 
 function splunk(regionInfo)
 {
-	var query = 'search index=canvas_' + regionInfo.index + ' global_root_account=' + globRootID + ' | search http_request=' + locationInfo + getMasqueradingQuery() + garbageRemoval;
-	targetURL = 'https://inst' + regionInfo.splunkBit + '.splunkcloud.com/en-US/app/canvas/search?q=' + encodeURIComponent(query);
-	var win = window.open(targetURL, '_blank');
+	const query = `search index=canvas_${regionInfo.index} global_root_account=${globRootID} http_request=${locationInfo} ${getMasqueradingQuery} ${garbageRemoval}`;
+	const targetURL = `https://inst${regionInfo.splunkBit}.splunkcloud.com/en-US/app/canvas/search?q=${encodeURIComponent(query)}`;
+	window.open(targetURL, '_blank');
 }
 
 function getMasqueradingQuery()
 {
-	return (masquerading == true) ? ' AND user_id=*' + currentUser : '';
+	return (masquerading) ? `AND user_id=*${currentUser}` : '';
 }
 
 function getPageInfo(url)
 {
-	var request = new XMLHttpRequest;
-	request.open("GET",url,!1);
+	let request = new XMLHttpRequest;
+	request.open('GET', url, !1);
 	request.send(null);
 
-	var xmlDoc = request.responseText;
+	const xmlDoc = request.responseText;
 
 	masquerading = /users\/\d+\/masquerade/g.test(xmlDoc);
 
 	currentUser = xmlDoc.match(/"current_user_id":"([^"]+)/)[1];
 
-	var header = request.getResponseHeader("x-canvas-meta");
+	const header = request.getResponseHeader("x-canvas-meta");
 
-	var region = header.match(/z=(\w+-\w+-\d)/)[1];
+	const region = header.match(/z=(\w+-\w+-\d)/)[1];
 
-	var s = header.match(/s=(\d+)/)[1];
-	var a = header.match(/a=(\d+)/)[1];
-	globRootID = s + "~" + a;
+	const s = header.match(/s=(\d+)/)[1];
+	const a = header.match(/a=(\d+)/)[1];
+	globRootID = `${s}~${a}`;
 
 	splunk(getRegionInfo(region));
 }
 
 function getRegionInfo(input)
 {
-	var output = [];
+	let output = [];
 	switch(input)
 	{
 		case 'us-east-1':
