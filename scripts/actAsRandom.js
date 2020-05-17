762e99a2202dd1ff53bc7064be77a3abc91e0698
Bounty.jpeg <jacobmayday@gmail.com>
2020-05-17 13:25:23 -0600

    Added ability to masquerade as random admin

--- scripts/actAsRandom.js
+++ scripts/actAsRandom.js
@@ -1,42 +1,128 @@
-chrome.storage.local.get(['masqueradingOptions'], data => {
+chrome.storage.local.get(['masqueradingOptions', 'ctoolhuAuthToken'], data => {
+    const authToken = data.ctoolhuAuthToken;
+	if (!authToken) {
+		alert('No auth token set');
+		return;
+	}
+
     let masqueradingOptions = data.masqueradingOptions;
 
     // Masquerade
     switch (masqueradingOptions.role) {
         case 'admin':
-            masqAsAdmin(masqueradingOptions.includedRoles);
+            masqAsAdmin(masqueradingOptions.includedRoles, authToken);
             break;
         case 'teacher':
-            masqAsTeacher();
+            masqAsTeacher(authToken);
             break;
         case 'student':
-            masqAsStudent();
+            masqAsStudent(authToken);
             break;
         case 'observer':
-            masqAsObserver();
+            masqAsObserver(authToken);
             break;
         case 'ta':
-            masqAsTA();
+            masqAsTA(authToken);
+            break;
+        case 'designer':
+            masqAsDesigner(authToken);
             break;
     }
 });
 
-function masqAsAdmin(options) {
-    console.log('admin');
+async function masqAsAdmin(options, authToken) {
+    let isFindingRootAdmins = options.find(element => element === 'rootAccountAdmins') != undefined;
+    let isFindingSubAdmins = options.find(element => element === 'subAccountAdmins') != undefined;
+    let findAllRoleTypes = options.find(element => element === 'allAdmins') != undefined;
+
+    let adminList = [];
+
+    // Grab root account admins
+    if (isFindingRootAdmins) {
+        adminList.push(...await getAdminsInAccount('self', findAllRoleTypes, authToken));
+    }
+
+    // Grab sub account admins
+    if (isFindingSubAdmins) {
+        const subAccount = await findSubAccountNumber(authToken);
+
+        if (subAccount) {
+            adminList.push(...await getAdminsInAccount(subAccount, findAllRoleTypes, authToken));
+        }
+    }
+
+    // Pick random admin and act as them
+    actAsUser(adminList[Math.floor(Math.random() * adminList.length)]);
 }
 
-function masqAsTeacher() {
+function masqAsTeacher(authToken) {
     console.log('teacher');
 }
 
-function masqAsStudent() {
+function masqAsStudent(authToken) {
     console.log('student');
 }
 
-function masqAsObserver() {
+function masqAsObserver(authToken) {
     console.log('observer');
 }
 
-function masqAsTA() {
+function masqAsTA(authToken) {
     console.log('ta');
+}
+
+function masqAsDesigner(authToken) {
+    console.log('designer');
+}
+
+function actAsUser(id) {
+    if (/\?/.test(document.URL)) {
+        window.location.href = `${document.URL}&become_user_id=${id}`;
+    } else {
+        window.location.href = `${document.URL}?become_user_id=${id}`;
+    }
+}
+
+async function getAdminsInAccount(account, findAllRoleTypes, authToken) {
+    let tempList = await paginate(`${document.location.origin}/api/v1/accounts/${account}/admins`, '', authToken);
+
+    if (findAllRoleTypes) {
+        // We only care about the User IDs
+        tempList = tempList.map(adminObj => adminObj.user.id);
+    } else {
+        // Only grab account admin types
+        tempList = tempList.filter(adminObj => adminObj.role_id === 1).map(adminObj => adminObj.user.id);
+    }
+
+    return tempList;
+}
+
+async function findSubAccountNumber(authToken) {
+    // Assume this is the right subaccount if we're in it
+    if (/accounts\/\d+/.test(document.location.pathname)) {
+        let id = document.location.pathname.match(/accounts\/(\d+)/)[1];
+
+        // Test to see if this is actually a subaccount
+        let accountAPI = await paginate(`${document.location.origin}/api/v1/accounts/${id}`, '', authToken);
+
+        if (accountAPI.root_account_id) {
+            return id;
+        } else {
+            return null;
+        }
+    } else if (/courses\/\d+/.test(document.location.pathname)) {
+        let courseID = document.location.pathname.match(/courses\/(\d+)/)[1];
+
+        // Get subaccount ID
+        let courseAPI = await paginate(`${document.location.origin}/api/v1/courses/${courseID}`, '', authToken);
+
+        // This is not in a subaccount
+        if (courseAPI.account_id === courseAPI.root_account_id) {
+            return null;
+        } else {
+            return courseAPI.account_id;
+        }
+    } else {
+        return null;
+    }
 }
